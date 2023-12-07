5fbb0b8565f76ce4823ea05743963b2bec6b4676
MercyNJ <mercinjuguna@gmail.com>
2023-12-07 09:38:15 +0000

    Defining variables with annotations

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Defining and annotating the following variables with the specified values:
+a, an int,value of 1
+pi, a float, value of 3.14
+i_understand_annotations, a boolean,value of True
+school, a string, value of “Holberton”
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
