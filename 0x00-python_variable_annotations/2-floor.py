8fb0fe9ddfb7678c6058df2ecccac040b2fede60
MercyNJ <mercinjuguna@gmail.com>
2023-12-07 09:24:30 +0000

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function add that takes a float as argS
+& returns the floor as an int.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    compute floor of a floating-point number.
+    """
+    return math.floor(n)
