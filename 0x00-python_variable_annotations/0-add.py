fdead5e1f1498afba3964db6bf3a4097161e24c1
MercyNJ <mercinjuguna@gmail.com>
2023-12-07 09:10:01 +0000

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function add that takes a float a and
+a float b as argS and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ Addition of 2 floats."""
+    return (a + b)
---
+++ README.md
@@ -0,0 +1,1 @@
+A repository containing Python Projects undertaken during the ALX's Software Engineering Program during the specialization phase in backend.
